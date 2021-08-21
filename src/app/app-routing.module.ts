abd7f5638dcbdcdbabddff38a90a9634b0a55fc2
Bernardo Issenguel <beissenguel12@gmail.com>
2021-08-21 21:01:58 +0100

    therd setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ViewComponent } from './view/view.component';
-import { AddComponent } from './add/add.component';
-import { EditComponent } from './edit/edit.component';
 
 const routes: Routes = [
-  { path: 'view', component: ViewComponent },
-  { path: 'add', component: AddComponent },
-  { path: 'edit/:id', component: EditComponent },
-  { path: '', component: ViewComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ViewComponent } from './view/view.component';
-import { AddComponent } from './add/add.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewComponent,
-    AddComponent,
-    EditComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
