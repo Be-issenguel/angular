a8b3821deb8466e6d28e55122729a91204c4f034
Bernardo Issenguel <beissenguel12@gmail.com>
2021-08-21 05:23:40 +0100

    second setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AddComponent } from './add/add.component';
-import { ViewComponent } from './view/view.component';
 
 const routes: Routes = [
-  { path: 'add', component: AddComponent },
-  { path: 'view', component: ViewComponent },
-  { path: '', component: ViewComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ViewComponent } from './view/view.component';
-import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewComponent,
-    AddComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
